94e0f09391b1c7bbe04ff50173d25ec3139e9304
Ferry To <ferry@verdantsparks.io>
2022-02-13 15:22:08 +0800

    add game lose logic and modify how answer is set

--- Game/Game.cs
+++ Game/Game.cs
@@ -1,71 +1,21 @@
 namespace EICS.WordleBlazor.Game;
 public class Game {
-    public readonly int Difficulty;
+    public readonly string Answer;
     public readonly List<GuessResult[]> Results = new();
     public event Action? GameUpdated;
     public bool IsWin { get; private set; }
+    public bool IsLose => !IsWin && Results.Count >= _maxAttempt;
 
-    private readonly string _answer;
-    public Game(string answer) {
-        _answer = answer;
-        Difficulty = _answer.Length;
+    private readonly int _maxAttempt;
+    public Game(string answer, int maxAttempt) {
+        Answer = answer;
+        _maxAttempt = maxAttempt;
     }
 
     public void Guess(string input) {
-        var guess = new Guess(_answer);
+        var guess = new Guess(Answer);
         Results.Add(guess.Match(input));
         IsWin = guess.Win();
         GameUpdated?.Invoke();
     }
-//
-//    private bool IndexInValidRange() => _currentInputIdx >= MinIndex && _currentInputIdx <= MaxIndex;
-//    private int MinIndex => 0;
-//    private int MaxIndex => Difficulty - 1;
-//
-//    public event Action? InputChanged;
-//    private readonly char[] _inputBuffer;
-//    private int _currentInputIdx;
-//    public char GetInput(int idx) {
-//        if (idx < 0) idx = 0;
-//        if (idx > Difficulty) idx = Difficulty;
-//        return _inputBuffer[idx];
-//    }
-//    private string Flush() {
-//        var text = string.Join(string.Empty, _inputBuffer);
-//
-//        for (var i = 0; i < _inputBuffer.Length; i++) {
-//            _inputBuffer[i] = ' ';
-//        }
-//        _currentInputIdx = MinIndex;
-//
-//        return text;
-//    }
-//    public void Input(char letter) {
-//        if(!IndexInValidRange()) return;
-//
-//        if(_inputBuffer[_currentInputIdx].Equals(' '))
-//            _inputBuffer[_currentInputIdx] = letter;
-//
-//        if (_currentInputIdx < MaxIndex) {
-//            _currentInputIdx += 1;
-//        }
-//
-//        InputChanged?.Invoke();
-//    }
-//
-//    public void Back() {
-//        if(!IndexInValidRange()) return;
-//
-//        if (_currentInputIdx > MinIndex) {
-//            if (_inputBuffer[_currentInputIdx].Equals(' ')) {
-//                _currentInputIdx -= 1;
-//                _inputBuffer[_currentInputIdx] = ' ';
-//            }
-//            else {
-//                _inputBuffer[_currentInputIdx] = ' ';
-//            }
-//        }
-//
-//        InputChanged?.Invoke();
-//    }
 }
