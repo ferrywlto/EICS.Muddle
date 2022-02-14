d7eba634ee5bdf044a671d73f1877809f1528a2b
Ferry To <ferry@verdantsparks.io>
2022-02-14 15:50:22 +0800

    Add keyboard button coloring based on guess result

--- Game/Game.cs
+++ Game/Game.cs
@@ -3,6 +3,7 @@ public class Game {
     public string Answer { get; private set; }
     private int MaxAttempt { get; set; }
     public readonly List<GuessResult[]> Results;
+    public readonly Dictionary<char, MatchResult> Distribution = new();
     public event Action? GameUpdated;
     public bool IsWin { get; private set; }
     public bool IsLose => !IsWin && Results.Count >= MaxAttempt && !Answer.Equals(string.Empty);
@@ -16,6 +17,7 @@ public class Game {
         Answer = answer;
         MaxAttempt = maxAttempt;
         Results.Clear();
+        Distribution.Clear();
         GameUpdated?.Invoke();
     }
 
@@ -23,8 +25,29 @@ public class Game {
         if(Answer.Equals(string.Empty)) return;
 
         var guess = new Guess(Answer);
-        Results.Add(guess.Match(input));
+        var guessResults = guess.Match(input);
+
+        MatchDistribution(guessResults);
+
+        Results.Add(guessResults);
         IsWin = guess.Win();
         GameUpdated?.Invoke();
+    }
+
+    private void MatchDistribution(IEnumerable<GuessResult> results) {
+        foreach ((var letter, var matchResult) in results) {
+            if (!Distribution.ContainsKey(letter)) {
+                Distribution[letter] = matchResult;
+                continue;
+            }
+
+            var matchType = Distribution[letter];
+
+            if(matchType == MatchResult.NoneHit && matchResult != MatchResult.NoneHit ||
+               matchType == MatchResult.CharHit && matchResult == MatchResult.FullHit)
+            {
+                Distribution[letter] = matchResult;
+            }
+        }
     }
 }
